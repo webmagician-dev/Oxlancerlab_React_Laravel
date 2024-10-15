9ab7436a84e93de6007dd0816c755df84c2d751d
devlegend524 <medina.devlegend524@gmail.com>
2024-10-15 13:15:23 -0700

    finished plans page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,12 @@
 import "../css/app.css";
+import "./bootstrap";
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@material-tailwind/react";
 import { MaterialTailwindControllerProvider } from "@/Context";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
@@ -21,6 +23,7 @@ createInertiaApp({
             <ThemeProvider>
                 <MaterialTailwindControllerProvider>
                     <App {...props} />
+                    <ToastContainer />
                 </MaterialTailwindControllerProvider>
             </ThemeProvider>
         );
